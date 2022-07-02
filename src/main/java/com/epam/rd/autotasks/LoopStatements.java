fadde1473dae65cdc7e2d0155a2efaaaf49e8e2c
Nataliia Koval <ruba.fish1500@gmail.com>
2022-07-02 20:15:58 +0300

    Fix LoopStatements

--- src/main/java/com/epam/rd/autotasks/LoopStatements.java
+++ src/main/java/com/epam/rd/autotasks/LoopStatements.java
@@ -1,13 +1,14 @@
 package com.epam.rd.autotasks;
 class LoopStatements {
     public static int task1(int n) {
-      int sum = 0;
-      int number = 0;
-      String digit = n + "";
-      for (int i=0; i<digit.length(); i++) {
-          number = digit.charAt(i)-'0';
-          if (number%2!=0) {
-              sum = sum + number;}
-      }   return sum;
-    }
+        int sum = 0;
+        if (n>0) {
+         int number1 = 0;
+         String digit = n + "";
+         for (int i = 0; i < digit.length(); i++) {
+             number1 = digit.charAt(i) - '0';
+             if (number1%2!= 0) {
+                 sum = sum + number1;}}
+         } return sum;
+     }
 }
